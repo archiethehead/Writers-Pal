ded85098328df180e1bdab6a16d14d9e3a913e75
Archie Healy <archiethehead@gmail.com>
2026-02-12 04:34:07 +0000

    ID Generator Collision and Script Init
    Feat: ID Generator now comes with a bool map for checking if values are occupied or not.
    
    Chore: Began setting up the constructor for ScriptProject.

--- Models/ScriptProject.cs
+++ Models/ScriptProject.cs
@@ -1,9 +1,11 @@
 ﻿using LibGit2Sharp;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Intrinsics.Arm;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using static System.Random;
 
@@ -12,9 +14,29 @@ namespace Writers_Pal.Models
 
     internal class ScriptProject {
 
-        public string projectName { get; set; } = "";
-        public string projectAuthor { get; set; } = "";
-        public Repository? currentRepo { get; set; }
+        public string projectName { get; private set; } = "";
+        public string projectAuthor { get; private set; } = "";
+        public List<TextElement> TextArray { get; private set; } = new();
+        public Repository? currentRepo { get; private set; }
+
+        public ScriptProject(string filepath, bool creating) {
+
+            if (creating) { CreateProject(filepath); }
+            else { OpenProject(filepath); }
+        
+        }
+
+        private void CreateProject(string filepath) { 
+        
+        
+        
+        }
+
+        private void OpenProject(string filepath) { 
+        
+
+
+        }
 
     }
 
@@ -23,40 +45,46 @@ namespace Writers_Pal.Models
         // Max Size of an unsigned, 16 bit int is 65535, going over this will result in countback
         // and ID corruption.
 
-        public const UInt16 ID_SIZE = 10000;
+        private const UInt16 ID_SIZE = 20000;
+        private static readonly bool[] assigned = new bool[ID_SIZE];
+        private static readonly Random rng = new();
 
 
-        static public UInt16 AssignID()
-            {
 
-                UInt16 newID;
+        static public UInt16 GenerateID() {
 
-                do
-                {
+            UInt16 newID;
 
-                    newID = RandomID();
+            do {
 
-                } while (IsIDTaken(newID));
+                newID = RandomID();
 
-                return newID;
+            } while (assigned[newID]);
 
-            }
+            AssignID(newID);
+            return newID;
 
-            static public UInt16 RandomID()
-            {
+        }
 
-                Random rng = new Random();
-                return (ushort)rng.Next((ushort)0, ID_SIZE);
+        public static void UnassignID(UInt16 ID) {
 
-            }
+            assigned[ID] = false;
+        
+        }
 
-            static public bool IsIDTaken(int ID)
-            {
+        public static void AssignID(UInt16 ID)
+        {
 
-                return false;
+            assigned[ID] = true;
 
-            }
+        }
+
+        static public UInt16 RandomID() {
+
+            return (ushort)rng.Next((ushort)0, ID_SIZE);
 
         }
 
     }
+
+}
